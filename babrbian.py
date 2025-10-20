ba64a90f29869fec38a8c0866b9072c5dc0e3a27
EliyaDastNeshan <elia.dastneshan@gmail.com>
2025-10-20 10:14:28 -0700

    give user ID

--- babrbian.py
+++ babrbian.py
@@ -2,9 +2,18 @@ from telebot import TeleBot
 from config import API_TOKEN
 
 bot = TeleBot (API_TOKEN)
+
+user_ID = []
 @bot.message_handler(commands = ['start'])
 def welcome (message):
     bot.send_message (message.chat.id,' خانه ای را که خداوند بنا نکند بنایانش زحمت بیهوده می کشند') 
+    if message.chat.id not in user_ID:
+        user_ID.append(message.chat.id)
+
+@bot.message_handler(commands=['خرید'])
+def send_update(message):
+    for id in user_ID:
+        bot.send_message (id,'ربات آماده سفارش است')
 
 
 
